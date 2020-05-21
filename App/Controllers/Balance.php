af7b75cbdb94b72c14cde331e80991773abdaac6
Bartosz Chodyła <bartoszchodyla@gmail.com>
2020-05-21 16:01:06 +0200

    Edit income modal completed

--- App/Controllers/Balance.php
+++ App/Controllers/Balance.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 use \Core\View;
 use \App\Models\Balances;
+use \App\Models\Incomes;
+use \App\Models\Expenses;
 use \App\Auth;
 use \App\Dates;
 use \App\Flash;
@@ -23,7 +25,7 @@ class Balance extends Authenticated
      */
     public function newAction()
     {	
-		if(!isset($_POST['periodOfTime'])){	
+			
 			View::renderTemplate('Balance/new.html', [
 			'incomeCategoriesAmount' => Balances::getIncomeCategoriesAmount(),	
 			'expenseCategoriesAmount' => Balances::getExpenseCategoriesAmount(),	
@@ -32,23 +34,12 @@ class Balance extends Authenticated
 			'todaysDate' => Dates::getTodaysDate(),
 			'yesterdaysDate' => Dates::getYesterdaysDate(),
 			'firstDate' => Balances::getFirstEchoDate(),
-			'secondDate' => Balances::getSecondEchoDate()
-			
+			'secondDate' => Balances::getSecondEchoDate(),
+			'userIncomes' => Incomes::getUserIncomeCategories(),
+			'userExpenses' => Expenses::getUserExpenseCategories(),
+			'paymentMethods' => Expenses::getUserPaymentMethods()		
 			]);
-		} else 
-		{
-			View::renderTemplate('Balance/new.html', [
-			'incomeCategoriesAmount' => Balances::getIncomeCategoriesAmount(),	
-			'expenseCategoriesAmount' => Balances::getExpenseCategoriesAmount(),	
-			'incomeCategoriesInDetail' => Balances::getIncomeCategoriesAmuntInDetail(),	
-			'expenseCategoriesInDetail' => Balances::getExpenseCategoriesAmuntInDetail(),
-			'todaysDate' => Dates::getTodaysDate(),
-			'yesterdaysDate' => Dates::getYesterdaysDate(),
-			'firstDate' => Balances::getFirstEchoDate(),
-			'secondDate' => Balances::getSecondEchoDate()
-			
-			]);
-		}
+
 		
 		
     }
